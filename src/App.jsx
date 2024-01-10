949e3bb4b189a31c11687a475dcb63fd50e2006d
waqas7658 <wa266867@gmail.com>
2024-01-10 11:49:59 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Auth/Login";
 import SignUp from "./Pages/Auth/SignUp";
-import Navbar from "./Components/navbar";
-
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <>
